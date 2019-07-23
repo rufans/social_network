4d5d8cffccb151b34a4818128e9ba4547913cd34
srufanov <sergey.rufanov@dataart.com>
2019-07-24 00:02:45 +0300

    Skeleton for userservice has been implemented

---
+++ userservice/src/main/java/com/srufanov/socialnetwork/userservice/config/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.srufanov.socialnetwork.userservice.config.exception;
+
+import com.srufanov.socialnetwork.userservice.exception.UserServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+@Slf4j
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserServiceException.class)
+    public final ResponseEntity<ExceptionResponse> handleProxBackendExceptions(UserServiceException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+                request.getDescription(false));
+        log.error(ex.getMessage(), ex);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ userservice/src/main/java/com/srufanov/socialnetwork/userservice/exception/UserServiceException.java
@@ -0,0 +1,15 @@
+package com.srufanov.socialnetwork.userservice.exception;
+
+public class UserServiceException extends RuntimeException {
+
+    public UserServiceException() {
+    }
+
+    public UserServiceException(String message) {
+        super(message);
+    }
+
+    public UserServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
