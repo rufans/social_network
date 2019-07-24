2076e1a9342bdf53c22e7d2c659f2ede81745c30
srufanov <sergey.rufanov@dataart.com>
2019-07-24 22:53:33 +0300

    userservice integration test configuration added

---
+++ userservice/src/test/java/com/srufanov/socialnetwork/userservice/integration/AbstractIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.srufanov.socialnetwork.userservice.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/db/clean-up.sql")
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected ObjectMapper mapper;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+}
