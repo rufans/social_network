0c86e9072c447cc5a9365bbd4a944c0471b9da5c
srufanov <sergey.rufanov@dataart.com>
2019-07-24 23:35:37 +0300

    userservice readme changed, small fixes done

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # social_network
 Test project with microservices architecture (Spring Boot, Spring Cloud, Docker)
+
+User service setup:
+
+1. create db users
+social_user
+social_user_password (change me)
+
+social_user_test
+social_user_test_password (change me)
+
+2. create databases that owned by above users:
+social_user_service (social_user)
+social_user_service_test (social_user_test)
+
+3. setup next system variables (change values to appropriate):
+DB_HOST = localhost
+DB_PORT = 5432
+US_DB_DATABASE = social_user_service
+US_DB_USER = social_user
+US_DB_PASSWORD = social_user_password
+---
+
+
+Post service setup:
+-
+
+
+Like service setup:
+-
--- userservice/src/main/java/com/srufanov/socialnetwork/userservice/config/exception/CustomizedResponseEntityExceptionHandler.java
+++ userservice/src/main/java/com/srufanov/socialnetwork/userservice/config/exception/CustomizedResponseEntityExceptionHandler.java
@@ -18,7 +18,7 @@ import java.util.Date;
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserServiceException.class)
-    public final ResponseEntity<ExceptionResponse> handleProxBackendExceptions(UserServiceException ex, WebRequest request) {
+    public final ResponseEntity<ExceptionResponse> handleUserServiceExceptions(UserServiceException ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
